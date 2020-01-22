7b3b29d8d531f2f0a559b7725cca888e71fd3eb0
Jeff Swisher <jefftswisher@gmail.com>
2020-01-22 19:23:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application 
   
-  @@items = [Item.new("Oranges", 4.50)
+  @@items = [Item.new("Oranges", 4.50)]
   
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new 
     
-    
+    if req.path.match(/)
     
     
   end 
